b8807110ecd28605785970278c23da4bdf8bc478
Mateus Santana <102470700+sstrangee@users.noreply.github.com>
2022-04-03 17:24:37 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
