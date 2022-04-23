eed7d819042f1d9b66adbac3a6ff88e8ba578cf1
Mateus Santana <102470700+sstrangee@users.noreply.github.com>
2022-04-23 10:16:10 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,10 +1,41 @@
 from flask import *
+import smtplib
+from email.message import EmailMessage
+from segredos import *
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/send', methods=['POST'])
+def send():
+    user = request.form["name"],
+    emaill = request.form["email"],
+    subject = request.form["subject"],
+    message = request.form["message"],
+    EMAIL_ADDRESS = email
+    EMAIL_PASSWORD = senha
+
+    msg = EmailMessage()
+    msg['subject'] = f"{user[0]}"
+    msg['from'] = 'ssseer'
+    msg['to'] = 'mateusdosummerblog@gmail.com'
+    msg.set_content(f'''
+    𝗔𝘀𝘀𝘂𝗻𝘁𝗼: {subject[0]}
+    
+𝗠𝗲𝗻𝘀𝘀𝗮𝗴𝗲𝗺: {message[0]}
+    
+    𝗘𝗺𝗮𝗶𝗹: {emaill[0]} ''')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+    return redirect('/')
+
 
 @app.route('/projetos')
 def projetos():
